90d652e83494fe498102da3dcca29d805d3ee613
Diego <diegovillarreal123@gmail.com>
2021-06-15 13:54:35 -0500

    deploying

--- routes/api.js
+++ routes/api.js
@@ -11,6 +11,7 @@ router.post("/api/transaction", ({body}, res) => {
     });
 });
 
+// api route
 router.post("/api/transaction/bulk", ({body}, res) => {
   Transaction.insertMany(body)
     .then(dbTransaction => {
